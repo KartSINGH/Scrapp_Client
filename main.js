bc05b38b32e82e17c73c3e36ca6f62bd07b4a3c0
kartik singh <kartiksingh@kartiks-MacBook-Air.local>
2017-03-18 19:05:32 +0530

    minor changes

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const express = require('express'),
     path = require('path'),
     app = express()
 app.use(express.static('.'));
-app.use('/', (request, response) => {
+app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'index.html'));
-    response.redirect('http://localhost:8889/#/');
 })
 
 app.listen(8889, 'localhost', () => {
