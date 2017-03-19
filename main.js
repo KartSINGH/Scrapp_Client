c7ccbf2c4d5a5cd49b49f470e5e41c49e4abad10
kartik singh <kartiksingh@kartiks-MacBook-Air.local>
2017-03-19 09:57:04 +0530

    public folder given permission

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express'),
     path = require('path'),
     app = express()
 app.use(express.static('.'));
+app.use(express.static('public'));
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'index.html'));
 })
